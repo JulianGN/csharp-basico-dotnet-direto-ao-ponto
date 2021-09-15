6fe2ef2a17d66356ff0f12c0b191e0c7cbd304c3
julianjgn <contato@juliannunes.com.br>
2021-09-14 21:07:19 -0300

    Final project Basis C# with POO

---
+++ POO-1/Estudante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_1
+{
+    public class Estudante : Pessoa
+    {
+        public Estudante(string turma, string nome, string documento, DateTime dataNascimento) : base(nome, documento, dataNascimento)
+        {
+            // com o : base(), estamos dizendo que usaremos as mesmas propriedades do original (Pessoa)
+            Turma = turma;
+            Notas = new List<int> { 5, 10, 4, 2, 3, 5 };
+        }
+        public string Turma { get; private set; }
+        public List<int> Notas { get; private set; }
+
+        public override void SeApresentar()
+        {
+            base.SeApresentar(); // puxa o próprio método original
+
+            var media = Notas.Average();
+
+            Console.WriteLine($"Sou um estudante da turma {Turma}, com Media de Nota: {media}");
+        }
+    }
+}
---
+++ POO-1/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_1
+{
+    public abstract class Pessoa
+    {
+        // Pessoa será a classe base e poderá ser herdada por outras
+        public Pessoa(string nome, string documento, DateTime dataNascimento)
+        {
+            Nome = nome;
+            Documento = documento;
+            DataNascimento = dataNascimento;
+        }
+
+        public string Nome { get; protected set; }
+        public string Documento { get; protected set; }
+        public DateTime DataNascimento { get; protected set; }
+
+        public virtual void SeApresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, Documento {Documento}, Data de Nascimento: {DataNascimento}");
+        }
+
+
+    }
+}
